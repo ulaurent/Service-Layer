a16a61a963d583fb06c9d00e087c26d4b03061f7
ulaurent <ulaurent12@gmail.com>
2024-01-25 18:28:57 -0500

    movie release date api's example

---
+++ Movies API/ApplicationApi/Controllers/MoviesController.cs
@@ -0,0 +1,54 @@
+﻿using ApplicationApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationApi.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // inject the database context 
+        // through the constructor of the controller
+        private readonly MovieContext _dbContext;
+
+        public MoviesController(MovieContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/Movies
+        [HttpGet("api/[controller]")]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+            if(_dbContext.Movies == null)
+            {
+                return NotFound();
+            }
+
+            return await _dbContext.Movies.ToListAsync();
+        }
+
+        //GET: api/Movies/5
+        [HttpGet("id")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            if(_dbContext.Movies == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _dbContext.Movies.FindAsync(id);
+
+            if(movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+    }
+}
---
+++ Movies API/ApplicationApi/Models/MovieContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationApi.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        // Entity set --> database table 
+        // Entity --> row within a table
+        public DbSet<Movie> Movies { get; set; } = null;
+    }
+}
